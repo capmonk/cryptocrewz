d2e13d25bd852eb15b1f0195e45d2d3752d31591
zayneanthonyrcd <zayne@rocketcitydigital.com>
2021-11-27 12:04:26 -0600

    Redesign Start

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import "./fonts/SF-UI-Display-Bold.otf";
-import "./fonts/SF-UI-Display-Ultralight.otf";
-import "./fonts/SF-UI-Text-Regular.otf";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
