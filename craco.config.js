91c1711067c8c2232b5c3419043db307b5740ebe
lundeee <lundeee@gmail.com>
2022-01-13 09:15:23 +0100

    splitChunks

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,37 @@
 module.exports = {
-	style: {
-		postcss: {
-			plugins: [
-				require("tailwindcss/nesting"),
-				require("tailwindcss"),
-				require("autoprefixer"),
-			],
-		},
-	},
+  style: {
+    postcss: {
+      plugins: [
+        require("tailwindcss/nesting"),
+        require("tailwindcss"),
+        require("autoprefixer"),
+      ],
+    },
+  },
+  webpack: {
+    configure: {
+      optimization: {
+        splitChunks: {
+          chunks: "async",
+          minSize: 20000,
+          minChunks: 5,
+          maxAsyncRequests: 30,
+          maxInitialRequests: 30,
+          enforceSizeThreshold: 50000,
+          cacheGroups: {
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      },
+    },
+  },
 };
